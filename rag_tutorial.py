d72daff685847046c48ae7f4e441a6f74e6226b5
ryanmark1867 <ryanmark2014@gmail.com>
2023-11-06 02:58:02 +0000

    rag cleanup

--- rag_tutorial.py
+++ rag_tutorial.py
@@ -1,3 +1,24 @@
+# from https://python.langchain.com/docs/integrations/vectorstores/pinecone
+# and https://python.langchain.com/docs/use_cases/question_answering/
+# adapted to use Vertex AI embeddings
+
+import os
+import getpass
+
+os.environ["PINECONE_API_KEY"] = getpass.getpass("Pinecone API Key:")
+os.environ["PINECONE_ENV"] = getpass.getpass("Pinecone Environment:")
+
+from langchain.embeddings import VertexAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Pinecone
+from langchain.document_loaders import TextLoader
+
+'''
+loader = TextLoader("../data/state_of_the_union.txt")
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+docs = text_splitter.split_documents(documents)
+'''
 # Load documents
 
 from langchain.document_loaders import WebBaseLoader
@@ -9,32 +30,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 0)
 splits = text_splitter.split_documents(loader.load())
 
-# Embed and store splits
-'''
-from langchain.vectorstores import Chroma
-from langchain.embeddings import OpenAIEmbeddings
-vectorstore = Chroma.from_documents(documents=splits,embedding=OpenAIEmbeddings())
-retriever = vectorstore.as_retriever()
-'''
-# from https://python.langchain.com/docs/integrations/vectorstores/pinecone
-
-# get credentials
-import os
-import getpass
-
-os.environ["PINECONE_API_KEY"] = getpass.getpass("Pinecone API Key:")
-os.environ["PINECONE_ENV"] = getpass.getpass("Pinecone Environment:")
-
-# import utilities
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import Pinecone
-from langchain.document_loaders import TextLoader
-
-# set embedding
-from langchain.embeddings import VertexAIEmbeddings
 embeddings = VertexAIEmbeddings()
-
-# set up pinecone
 import pinecone
 
 # initialize pinecone
@@ -43,6 +39,49 @@ pinecone.init(
     environment=os.getenv("PINECONE_ENV"),  # next to api key in console
 )
 
-index_name = "langchain-demo"
-index = pinecone.Index("langchain-demo")
-vectorstore = Pinecone(index, embeddings.embed_query, "text")
+index_name = "rag-demo"
+
+# First, check if our index already exists. If it doesn't, we create it
+if index_name not in pinecone.list_indexes():
+    # we create a new index
+    pinecone.create_index(
+      name=index_name,
+      metric='cosine',
+      dimension=768  
+)
+
+#vectorstore = Pinecone(index_name, embeddings.embed_query, splits)
+print("past index creation for index ", index_name)
+# Vertex AI embedding model  uses 768 dimensions`
+vectorstore = Pinecone.from_documents(splits, embeddings, index_name=index_name)
+
+retriever = vectorstore.as_retriever()
+
+# Prompt 
+# https://smith.langchain.com/hub/rlm/rag-prompt
+
+from langchain import hub
+rag_prompt = hub.pull("rlm/rag-prompt")
+
+print("past index rag_prompt creation ")
+#from langchain.chat_models import ChatVertexAI
+from langchain.prompts import ChatPromptTemplate
+#llm = ChatVertexAI()
+from langchain.llms import VertexAI
+llm = VertexAI()
+ 
+
+# RAG chain 
+
+from langchain.schema.runnable import RunnablePassthrough
+rag_chain = (
+    {"context": retriever, "question": RunnablePassthrough()} 
+    | rag_prompt 
+    | llm 
+)
+
+rci_output = rag_chain.invoke("What is Task Decomposition?")
+print("rci_output is: ",rci_output)
+
+
+
